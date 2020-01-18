7b7434c9581680ab4d3fecf5d34206d8b0aab7d0
qiacheng <choliaky@gmail.com>
2020-01-18 12:22:13 -0800

    Updates

---
+++ precache-manifest.54632715a5d127c190539431c77698b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b8c2eef1bf559e280d17111ea226dd3",
+    "url": "/yelp-data-analysis/index.html"
+  },
+  {
+    "revision": "25922b580fe821f6130c",
+    "url": "/yelp-data-analysis/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "a272ede52ab8ef966dd0",
+    "url": "/yelp-data-analysis/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "25922b580fe821f6130c",
+    "url": "/yelp-data-analysis/static/js/2.f73632d5.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/yelp-data-analysis/static/js/2.f73632d5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a272ede52ab8ef966dd0",
+    "url": "/yelp-data-analysis/static/js/main.100c5787.chunk.js"
+  },
+  {
+    "revision": "81153907c299d71d3e29",
+    "url": "/yelp-data-analysis/static/js/runtime-main.f0c598f1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/yelp-data-analysis/precache-manifest.222555ce36ff080731d63884fd8637ee.js"
+  "/yelp-data-analysis/precache-manifest.54632715a5d127c190539431c77698b7.js"
 );
 
 self.addEventListener('message', (event) => {
