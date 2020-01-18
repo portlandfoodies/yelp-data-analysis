39243eadb40b766f89e7c23da0933c28506984d2
qiacheng <choliaky@gmail.com>
2020-01-18 10:20:28 -0800

    Updates

---
+++ precache-manifest.a9606218f3cfa9508400caf5c3e9dda2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4bcfa56dc928d30654feec4d089be25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "156f08fa865c852b4568",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e796e2b5740fb2de241a",
+    "url": "/static/js/2.956b3604.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.956b3604.chunk.js.LICENSE"
+  },
+  {
+    "revision": "156f08fa865c852b4568",
+    "url": "/static/js/main.c5977d38.chunk.js"
+  },
+  {
+    "revision": "2346c3965dc23aaecb85",
+    "url": "/static/js/runtime-main.04fa72fc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
