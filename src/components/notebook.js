347a5b40a8502c5bcf4024c3a460f3d95e2ffbe3
qiacheng <choliaky@gmail.com>
2020-03-17 01:10:12 -0700

    final commit

--- src/components/notebook.js
+++ src/components/notebook.js
@@ -1,49 +1,24 @@
 import React, { Component } from 'react'
 import ReactMarkdown from 'react-markdown'
 import {Container} from 'react-bootstrap'
-import notebook from '../md/notebook.md'
-export default class Notebook extends Component {
-  constructor(){
-    super()
-    this.state ={
-      markdown : ''
-    }
-  }
-  componentDidMount(){
-    const readmefile = notebook
-    fetch(readmefile).then(response => {
-      return response.text()
-    }).then(text => {
-      this.setState({
-        markdown : text
-      }
-      )
-    })
-  }
+import notebook from '../notebook.pdf'
 
-  render(){
-    const {markdown} = this.state
-    return(
-    
-      
-      <div>
-        <br />
-        <br />
-        <br />
-        <br />
+
+export default class Notebook extends Component {
+
+  
+ 
+  render() {
+
+ 
+    return (
+      <Container>
         
-        <Container>
-          <p align='center'><body>
-            <p><a href="https://github.com/portlandfoodies/portlandfoodies.github.io">Project Github Repo</a></p>
-            <p><a href="https://github.com/portlandfoodies/yelp-data-analysis">Project Webpage Repo</a></p>
-            <br />
-            <ReactMarkdown source={markdown}/>
-            </body>
-          </p>
-        </Container>
-      </div>
+      <iframe style={{ left: '120px', width: '100%',height: '2000px' }} src={notebook} />
       
-    )
+      </Container>
+      
+    );
   }
 
 }
